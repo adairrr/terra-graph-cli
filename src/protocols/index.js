34e9f3d2327ddfaf0620a23a88b1b043c6c84c20
Patrycja Zawadka <patrycja@figment.io>
2021-10-26 23:53:59 +0200

    Delete codegen template for near and tendermint

--- src/protocols/index.js
+++ src/protocols/index.js
@@ -1,7 +1,6 @@
 const immutable = require('immutable')
 const EthereumTypeGenerator = require('./ethereum/type-generator')
 const EthereumTemplateCodeGen = require('./ethereum/codegen/template')
-const NearTemplateCodeGen = require('./near/codegen/template')
 const EthereumABI = require('./ethereum/abi')
 const EthereumSubgraph = require('./ethereum/subgraph')
 const NearSubgraph = require('./near/subgraph')
@@ -60,10 +59,6 @@ module.exports = class Protocol {
       case 'ethereum':
       case 'ethereum/contract':
         return new EthereumTemplateCodeGen(template)
-      case 'near':
-        return new NearTemplateCodeGen(template)
-      case 'tendermint':
-        return new NearTemplateCodeGen(template)
       default:
         throw new Error(
           `Data sources with kind '${this.name}' are not supported yet`,
