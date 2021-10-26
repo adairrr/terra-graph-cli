73c8a6740a57b39ad42a02e93f8d6d9a5f833ca7
Patrycja Zawadka <patrycja@figment.io>
2021-10-26 23:18:32 +0200

    Add tendermint to protocols

--- src/protocols/index.js
+++ src/protocols/index.js
@@ -1,34 +1,36 @@
 const immutable = require('immutable')
 const EthereumTypeGenerator = require('./ethereum/type-generator')
 const EthereumTemplateCodeGen = require('./ethereum/codegen/template')
+const NearTemplateCodeGen = require('./near/codegen/template')
 const EthereumABI = require('./ethereum/abi')
 const EthereumSubgraph = require('./ethereum/subgraph')
 const NearSubgraph = require('./near/subgraph')
-
+​
 module.exports = class Protocol {
   static fromDataSources(dataSourcesAndTemplates) {
     const firstDataSourceKind = dataSourcesAndTemplates[0].kind
     return new Protocol(firstDataSourceKind)
   }
-
+​
   constructor(name) {
     this.name = this.normalizeName(name)
   }
-
+​
   availableProtocols() {
     return immutable.fromJS({
       // `ethereum/contract` is kept for backwards compatibility.
       // New networks (or protocol perhaps) shouldn't have the `/contract` anymore (unless a new case makes use of it).
       ethereum: ['ethereum', 'ethereum/contract'],
       near: ['near'],
+      tendermint: ['tendermint/data'],
     })
   }
-
+​
   normalizeName(name) {
     return this.availableProtocols()
       .findKey(possibleNames => possibleNames.includes(name))
   }
-
+​
   // Receives a data source kind, and checks if it's valid
   // for the given protocol instance (this).
   isValidKindName(kind) {
@@ -36,17 +38,19 @@ module.exports = class Protocol {
       .get(this.name, immutable.List())
       .includes(kind)
   }
-
+​
   hasABIs() {
     switch (this.name) {
       case 'ethereum':
       case 'ethereum/contract':
         return true
-      case 'near':
+      case 'near/data':
+        return false
+      case 'tendermint/data':
         return false
     }
   }
-
+​
   getTypeGenerator(options) {
     switch (this.name) {
       case 'ethereum':
@@ -54,21 +58,27 @@ module.exports = class Protocol {
         return new EthereumTypeGenerator(options)
       case 'near':
         return null
+      case 'tendermint':
+        return null
     }
   }
-
+​
   getTemplateCodeGen(template) {
     switch (this.name) {
       case 'ethereum':
       case 'ethereum/contract':
         return new EthereumTemplateCodeGen(template)
+      case 'near':
+        return new NearTemplateCodeGen(template)
+      case 'tendermint':
+        return new NearTemplateCodeGen(template)
       default:
         throw new Error(
-          `Template data sources with kind '${this.name}' are not supported yet`,
+          `Data sources with kind '${this.name}' are not supported yet`,
         )
     }
   }
-
+​
   getABI() {
     switch (this.name) {
       case 'ethereum':
@@ -76,22 +86,22 @@ module.exports = class Protocol {
         return EthereumABI
       case 'near':
         return null
+      case 'tendermint':
+        return null
     }
   }
-
+​
   getSubgraph(options = {}) {
     const optionsWithProtocol = { ...options, protocol: this }
-
+​
     switch (this.name) {
       case 'ethereum':
       case 'ethereum/contract':
         return new EthereumSubgraph(optionsWithProtocol)
       case 'near':
         return new NearSubgraph(optionsWithProtocol)
-      default:
-        throw new Error(
-          `Data sources with kind '${this.name}' are not supported yet`,
-        )
+      case 'tendermint':
+        return new NearSubgraph(optionsWithProtocol)
     }
   }
 }
